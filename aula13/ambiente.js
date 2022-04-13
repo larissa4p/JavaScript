5463c41f024a450c1de55598f1802ca44581787b
Larissa <larissabessa4@gmail.com>
2022-04-13 11:49:17 -0300

    -

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+var c = 1 
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6 )
+
+
+
+
+
+
+/*
+var c = 1
+while (c <= 5) {
+    console.log(`FATIA ${c}`)
+    c++ // c = c + 1
+}
+
+
+
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+*/
