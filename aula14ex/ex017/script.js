f143e80288acfffc6ea51ead9b2db06b964c5361
Larissa <larissabessa4@gmail.com>
2022-04-17 17:41:40 -0300

    -

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,10 @@
+function tabuada() {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        var n = Number(num.value)
+        
+    }
+}
