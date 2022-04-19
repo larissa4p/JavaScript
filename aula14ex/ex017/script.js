09b21f3c6d2f010ca0a3f8385f6375c8a96ff9fd
Larissa <larissabessa4@gmail.com>
2022-04-19 15:31:52 -0300

    -

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -5,6 +5,13 @@ function tabuada() {
         window.alert('Por favor, digite um número!')
     } else {
         var n = Number(num.value)
-        
+        var c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }
     }
 }
